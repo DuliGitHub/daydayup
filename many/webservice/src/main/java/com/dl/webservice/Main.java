4ff5ef62f8fd27e33a401531966273553ac4b0a4
duli <duli@iwisedev.com>
2018-08-14 22:00:26 +0800

    webservers

---
+++ many/webservice/src/main/java/com/dl/webservice/Main.java
@@ -0,0 +1,30 @@
+package com.dl.webservice;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * ━━━━━━如来保佑━━━━━━
+ * ━━━━━━如来保佑━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━永无BUG━━━━━━
+ *
+ * @author Ledev4
+ * @date 2018/8/14 15:54 星期二
+ */
+
+@Component
+public class Main {
+    static {
+
+    }
+}
