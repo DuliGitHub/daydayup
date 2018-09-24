726be192994b851eecf6ac0aeffdf6688a8b8c74
duli <592306832@qq.com>
2018-09-24 23:36:04 +0800

    singleton -static

---
+++ many/juc02/src/main/java/com/multi_thread/singleton/singleton_static/MyObject.java
@@ -0,0 +1,16 @@
+package com.multi_thread.singleton.singleton_static;
+
+public class MyObject {
+    private static MyObject instance = null;
+
+    public MyObject() {
+
+    }
+    static {
+        instance = new MyObject();
+    }
+
+    public static MyObject getInstance(){
+        return instance;
+    }
+}
