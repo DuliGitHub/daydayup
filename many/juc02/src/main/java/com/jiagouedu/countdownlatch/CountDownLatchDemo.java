457cd280cd5c15b0c4d1bf9978e3ba0b4f87b127
duli <592306832@qq.com>
2018-09-30 22:36:00 +0800

    countdownlatch

---
+++ many/juc02/src/main/java/com/jiagouedu/countdownlatch/CountDownLatchDemo.java
@@ -0,0 +1,35 @@
+package com.jiagouedu.countdownlatch;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CountDownLatchDemo implements Runnable {
+    static final CountDownLatch end = new CountDownLatch(10);
+    static  CountDownLatchDemo demo = new CountDownLatchDemo();
+
+    @Override
+    public void run() {
+        try {
+            //模拟检查任务
+            Thread.sleep(new Random().nextInt(10)*1000);
+            System.out.println("check complete");
+            end.countDown();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException{
+        ExecutorService exec = Executors.newFixedThreadPool(10);
+        for(int i = 0; i< 10;i++){
+            exec.submit(demo);
+        }
+        //等待检查
+        end.await();//要求主线程等待所有10个检查任务全部完成
+        //发射火箭
+        System.out.println("Fire !");
+        exec.shutdown();
+    }
+}
