40f8925bfb1d3fb11ce305f53ab93fb973c9bd40
592306832@qq.com <592306832@qq.com>
2018-10-09 18:43:08 +0800

    juc

--- many/juc05/src/main/java/com/jiagouedu/concurrent2/ExecutorServer.java
+++ many/juc05/src/main/java/com/jiagouedu/concurrent2/ExecutorServer.java
@@ -1,20 +1,4 @@
-package com.jiagouedu.concurrent2;/* ━━━━━━如来保佑━━━━━━
- * 　　　┏┓　　　┏┓
- * 　　┏┛┻━━━┛┻┓
- * 　　┃　　　━　　　┃
- * 　　┃　┳┛　┗┳　┃
- * 　　┃　　　┻　　　┃
- * 　　┗━┓　　　┏━┛
- * 　　　　┃　　　┗━━━┓
- * 　　　　┃　　　　　　　┣┓
- * 　　　　┃　　　　　　　┏┛
- * 　　　　┗┓┓┏━┳┓┏┛
- * 　　　　　┗┻┛　┗┻┛
- * ━━━━━━永无BUG━━━━━━
- * 图灵学院-悟空老师
- * 以往视频加小乔老师QQ：895900009
- * 悟空老师QQ：245553999
- */
+package com.jiagouedu.concurrent2;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
@@ -22,7 +6,7 @@ import java.util.concurrent.Future;
 public interface ExecutorServer  extends  Executor{
 
 
-   public void shutDown();
-   public <T>Future<T> submit(Runnable runnable);
-   public <T>Future<T> submit(Callable callable);
+    void shutDown();
+    <T>Future<T> submit(Runnable runnable);
+    <T>Future<T> submit(Callable callable);
 }
