cd488ff80d1895b6aee18aa04afe64a1ed1768f8
592306832@qq.com <592306832@qq.com>
2018-10-21 22:48:06 +0800

    about jvm

---
+++ ninja/src/main/java/com/kawasaki/jvm/TestDynamicLoad.java
@@ -0,0 +1,26 @@
+package com.kawasaki.jvm;
+
+public class TestDynamicLoad {
+    static {
+        System.out.println("*************static code************");
+    }
+
+    public static void main(String[] args){
+        new A();
+        System.out.println("*************load test************");
+        new B();
+    }
+}
+
+class A{
+    public A(){
+        System.out.println("*************initial A************");
+    }
+}
+
+class B{
+    public B(){
+        System.out.println("*************initial B************");
+    }
+}
+
