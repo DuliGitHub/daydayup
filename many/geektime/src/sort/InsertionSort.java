d259f0aa41a789c02b91da12904a231b3381fc75
592306832@qq.com <592306832@qq.com>
2018-11-04 23:50:08 +0800

    insertionSort

---
+++ many/geektime/src/sort/InsertionSort.java
@@ -0,0 +1,36 @@
+package sort;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] a = {22,13,5,12,343,22,453,235,236,124};
+        insertionSort(a,10);
+        for(int i = 0;i  < a.length;i++){
+            System.out.print(a[i] + " - ");
+        }
+    }
+
+    /**
+     *插入排序
+     * @param a 数组
+     * @param n 数组大小
+     */
+    public static void insertionSort(int[] a,int n){
+        if(n <= 1){
+            return;
+        }
+
+        for(int i = 1;i < n; ++ i){
+            int value = a[i];
+            int j = i - 1;
+            //查找插入的位置
+            for(; j >= 0; --j){
+                if(a[j] > value){
+                    a[j+1] = a[j]; //数据移动
+                }else {
+                    break;
+                }
+            }
+            a[j+1] = value;
+        }
+    }
+}
